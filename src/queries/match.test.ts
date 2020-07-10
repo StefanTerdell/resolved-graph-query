130720071c458f459e10a9744c2fe437e553d5df
Stefan Terdell <stefan.terdell@live.com>
2020-07-11 01:31:46 +0200

    :fire: needs more object maps! :fire:

--- src/queries/match.test.ts
+++ src/queries/match.test.ts
@@ -1,5 +1,6 @@
 import { linear4 } from './testGraphs/linear4'
 import { matchQuery } from '..'
+import { circular3 } from './testGraphs/circular3'
 
 describe('When traversing a linear graph', () => {
   it('should be possible to do a simple match query', () => {
@@ -20,9 +21,16 @@ describe('When traversing a linear graph', () => {
   })
 
   it('should be possible to perform a recursive query', () => {
-    const result = matchQuery('{id:"A"}-3{}>{data:{hello: "world!"}}', linear4)
+    const result = matchQuery('{id:"A"}-{recurse:3}>{data:{hello: "world!"}}', linear4)
     expect(result[0].from[0].to.from[0].to.from[0].to).toMatchObject({
       data: { hello: 'world!' },
     })
+  })
+})
+
+describe('When traversing a circular graph', () => {
+  it('should recursively find its way trough a circular monodirectional graph', () => {
+    const result = matchQuery('{id: "A"}-{recurse:2, data: {type: "clockwise"}}>{id: "A"}', circular3)
+    expect(result[0]).toBe(result[0].from[0].to.from[0].to.from[0].to)
   })
 })
---
+++ src/queries/testGraphs/circular3.ts
@@ -0,0 +1,70 @@
+import { ResolvedGraph } from 'resolved-graph'
+
+/**
+ * This graph contains three nodes (A, B, C) six links (1-6)
+ * The nodes are first linked A to B, B to C and C back to A, creating a circular structure. These links have data: { type: "clockwise"}
+ * THe nodes are then linked A to C, C to B and B back to A. These links have data: { type: "counter-clockwise" }
+ */
+export const circular3 = new ResolvedGraph({
+  nodes: [
+    {
+      id: 'A',
+    },
+    {
+      id: 'B',
+    },
+    {
+      id: 'C',
+    },
+  ],
+  links: [
+    {
+      id: '1',
+      from: 'A',
+      to: 'B',
+      data: {
+        type: 'clockwise',
+      },
+    },
+    {
+      id: '2',
+      from: 'B',
+      to: 'C',
+      data: {
+        type: 'clockwise',
+      },
+    },
+    {
+      id: '3',
+      from: 'C',
+      to: 'A',
+      data: {
+        type: 'clockwise',
+      },
+    },
+    {
+      id: '4',
+      from: 'A',
+      to: 'C',
+      data: {
+        type: 'counter-clockwise',
+      },
+    },
+    {
+      id: '5',
+      from: 'C',
+      to: 'B',
+      data: {
+        type: 'counter-clockwise',
+      },
+    },
+    {
+      id: '6',
+      from: 'B',
+      to: 'A',
+      data: {
+        type: 'counter-clockwise',
+      },
+    },
+  ],
+})
